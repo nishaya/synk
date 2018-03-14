36d856facdbc1d453991a8b50f11d38c470a6c87
nishaya <nishaya@users.noreply.github.com>
2018-03-14 13:18:47 +0900

    notification

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,29 @@ import * as ReactDOM from 'react-dom';
 import App from 'App';
 import registerServiceWorker from 'registerServiceWorker';
 import 'index.css';
+import * as firebase from 'firebase';
+
+const { REACT_APP_FIREBASE_API_KEY: apiKey, REACT_APP_FIREBASE_SENDER_ID: messagingSenderId } = process.env;
+console.log(apiKey, messagingSenderId);
+
+firebase.initializeApp(
+  {
+    apiKey, messagingSenderId
+  }
+);
+
+const messaging = firebase.messaging();
+console.log(messaging);
+
+(messaging.requestPermission() as Promise<any>)
+.then(function() {
+  console.log('Notification permission granted.');
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  // ...
+})
+.catch(function(err: any) {
+  console.log('Unable to get permission to notify.', err);
+});
 
 ReactDOM.render(
   <App />,
