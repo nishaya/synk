4d0a0036ea8881f7af428d56587d109023a50613
nishaya <nishaya@users.noreply.github.com>
2018-03-15 09:52:27 +0900

    mock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import * as React from 'react';
-import './App.css';
-import Pattern from 'Components/Pattern';
-
-const logo = require('./logo.svg');
+import * as React from 'react'
+import './App.css'
+import Mock from 'Components/Mock'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Pattern />
+        <Mock />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Components/Mock.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Session from 'Components/Session'
+
+export default () => (
+  <div>
+    Mock<Session />
+  </div>
+)
