9d1441d661963ad595e5f9f02a3fd17d1a85a160
nishaya <nishaya@users.noreply.github.com>
2018-03-15 18:15:34 +0900

    stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import './App.css'
 import Mock from 'Components/Mock'
+import Stats from 'Components/Stats'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Mock />
+        <Stats />
       </div>
     )
   }
