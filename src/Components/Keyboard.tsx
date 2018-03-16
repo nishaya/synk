308e843743955aa87bffb0b966a5c45c5c4a68cd
nishaya <nishaya@users.noreply.github.com>
2018-03-16 16:42:24 +0900

    click handler

--- src/Components/Keyboard.tsx
+++ src/Components/Keyboard.tsx
@@ -7,20 +7,38 @@ const MAX_DEFAULT = 100
 interface Props {
   min?: number
   max?: number
+  onKey?: (keyNumber: number) => void
 }
 
 const Frame = styled.div`
   border: 1px solid #333;
 `
 
-export const Keyboard = ({ min = MIN_DEFAULT, max = MAX_DEFAULT }: Props) => {
+const Key = styled.div`
+  background-color: #fff;
+  border: 1px solid #333;
+  width: 24px;
+  height: 80px;
+  display: inline-block;
+`
+
+export const Keyboard = ({
+  min = MIN_DEFAULT,
+  max = MAX_DEFAULT,
+  onKey = (keyNumber: number) => console.log('onKey', keyNumber)
+}: Props) => {
   return (
     <div>
       <h2>Keyboard</h2>
       <Frame>
-        {new Array(max - min)
-          .fill(null)
-          .map((_: number, i: number) => <div key={i}>{i}</div>)}
+        {new Array(max - min).fill(null).map((_: number, i: number) => {
+          const keyNumber = i + min
+          return (
+            <Key key={i} onClick={() => onKey(keyNumber)}>
+              {keyNumber}
+            </Key>
+          )
+        })}
       </Frame>
     </div>
   )
