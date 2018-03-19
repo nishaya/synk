f8789e99ff963fb90fbfff604e2596e16570e508
nishaya <nishaya@users.noreply.github.com>
2018-03-19 14:04:43 +0900

    block select

--- src/Components/Session.tsx
+++ src/Components/Session.tsx
@@ -1,13 +1,25 @@
 import ArrangementComponent from 'Components/Arrangement'
 import BlockComponent from 'Components/Block'
 import BlockEditComponent from 'Components/BlockEdit'
+import MenuItem from 'material-ui/MenuItem'
 import RaisedButton from 'material-ui/RaisedButton'
+import SelectField from 'material-ui/SelectField'
 import * as React from 'react'
 import { Block, Session } from 'types'
 
 interface Props {
   session: Session
 }
+
+const BlockSelect = ({ blocks }: { blocks: Block[] }) => (
+  <SelectField>
+    {blocks.map((block: Block) => (
+      <MenuItem key={block.id} value={block.id}>
+        {block.name}
+      </MenuItem>
+    ))}
+  </SelectField>
+)
 
 export default ({ session }: Props) => (
   <div style={{ padding: '4px' }}>
@@ -19,6 +31,7 @@ export default ({ session }: Props) => (
         <BlockComponent block={block} />
       </div>
     ))}
+    <BlockSelect blocks={session.blocks} />
     <BlockEditComponent blocks={session.blocks} />
     <ArrangementComponent arrangement={session.arrangement} />
   </div>
