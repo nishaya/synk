e8b05db76d11f70078fa8b9cf36c278c7c05cc91
nishaya <nishaya@users.noreply.github.com>
2018-03-20 09:45:57 +0900

    preset types

--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,31 @@ export interface SynthPreset {
   type: SynthPresetType
 }
 
+export function isOscSynthPreset(v: any): v is OscSynthPreset {
+  return v.type === 'osc'
+}
+
+export function isFmSynthPreset(v: any): v is FmSynthPreset {
+  return v.type === 'fm'
+}
+
+export function isDrumsSynthPreset(v: any): v is DrumsSynthPreset {
+  return v.type === 'drums'
+}
+
+export interface OscSynthPreset extends SynthPreset {
+  type: 'osc'
+  oscillator: OscillatorType
+}
+
+export interface FmSynthPreset extends SynthPreset {
+  type: 'fm'
+}
+
+export interface DrumsSynthPreset extends SynthPreset {
+  type: 'drums'
+}
+
 export interface Arrangement {
   blocks: BlockInfo[]
 }
