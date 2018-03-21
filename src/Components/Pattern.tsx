17caeb30eddc81a010652493fb3aa53688212a37
nishaya <nishaya@users.noreply.github.com>
2018-03-21 17:26:54 +0900

    scrollable

--- src/Components/Pattern.tsx
+++ src/Components/Pattern.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Pattern } from 'types'
-
 interface Props {
   pattern: Pattern
 }
@@ -13,6 +12,11 @@ interface State {
 class PatternComponent extends React.Component<Props, State> {
   divElement: HTMLDivElement
 
+  state: State = {
+    stageWidth: 0,
+    stageHeight: 0
+  }
+
   componentDidMount() {
     const stageHeight = this.divElement.clientHeight
     const stageWidth = this.divElement.clientWidth
@@ -21,13 +25,22 @@ class PatternComponent extends React.Component<Props, State> {
 
   render() {
     const { pattern } = this.props
+    const { stageHeight, stageWidth } = this.state
     console.log(this.state)
     return (
       <div
         style={{ minHeight: '100%' }}
         ref={(div: HTMLDivElement) => (this.divElement = div)}
       >
-        <pre>{JSON.stringify(pattern.notes, null, 4)}</pre>
+        <div
+          style={{
+            overflow: 'auto',
+            width: `${stageWidth}px`,
+            height: `${stageHeight}px`
+          }}
+        >
+          <pre>{JSON.stringify(pattern.notes, null, 4)}</pre>
+        </div>
       </div>
     )
   }
