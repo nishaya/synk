f525b04a48b3687b6f7ba64b38f771be07179b16
nishaya <nishaya@users.noreply.github.com>
2018-03-22 00:58:06 +0900

    show notes

--- src/Components/Pattern.tsx
+++ src/Components/Pattern.tsx
@@ -19,6 +19,11 @@ const avgNotes = (notes: Note[]): number => {
   )
 }
 
+const noteHeight = 8
+const displayNotes = 48
+const beatWidth = 32
+const durationWidth = beatWidth / 480
+
 class PatternComponent extends React.Component<Props, State> {
   divElement: HTMLDivElement
 
@@ -36,8 +41,9 @@ class PatternComponent extends React.Component<Props, State> {
   render() {
     const { pattern } = this.props
     const { stageHeight, stageWidth } = this.state
-    console.log(this.state)
-    console.log('average', avgNotes(pattern.notes))
+    const avg = avgNotes(pattern.notes)
+    const maxNote = avg + displayNotes / 2
+    const minNote = maxNote - displayNotes
     return (
       <div
         style={{ minHeight: '100%' }}
@@ -50,6 +56,27 @@ class PatternComponent extends React.Component<Props, State> {
             height: `${stageHeight}px`
           }}
         >
+          <svg
+            id="grid"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={`0 0 ${stageWidth} ${displayNotes * noteHeight}`}
+            preserveAspectRatio="xMidYMid meet"
+          >
+            {pattern.notes.map((note: Note, index: number) => {
+              if (note.note < minNote || maxNote < note.note) {
+                return null
+              }
+              return (
+                <rect
+                  key={`note_${index}`}
+                  x={index * beatWidth}
+                  y={(maxNote - note.note) * noteHeight}
+                  width={durationWidth * note.duration}
+                  height={noteHeight}
+                />
+              )
+            })}
+          </svg>
           <pre>{JSON.stringify(pattern.notes, null, 4)}</pre>
         </div>
       </div>
