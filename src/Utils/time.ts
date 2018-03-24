0ac21cb760cebeaad3d618ea1cc0946b36f530ed
nishaya <nishaya@users.noreply.github.com>
2018-03-24 20:48:43 +0900

    001:01:000

--- src/Utils/time.ts
+++ src/Utils/time.ts
@@ -4,8 +4,8 @@ export const BEAT_LENGTH = 480
 
 export const position2time = (time: number, bpb: number = 4): string => {
   const barLength = bpb * BEAT_LENGTH
-  const bars = ~~(time / barLength)
-  const beats = ~~((time % barLength) / BEAT_LENGTH)
+  const bars = ~~(time / barLength) + 1
+  const beats = ~~((time % barLength) / BEAT_LENGTH) + 1
   const steps = time % BEAT_LENGTH
   return sprintf('%03d:%02d:%03d', bars, beats, steps)
 }
