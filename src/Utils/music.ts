0e243c499d08c8df601f459a1a34325f47bffd04
nishaya <nishaya@users.noreply.github.com>
2018-03-24 13:21:50 +0900

    music util

---
+++ src/Utils/music.ts
@@ -0,0 +1,28 @@
+export const roots = [
+  'C',
+  'Cb',
+  'D',
+  'Db',
+  'E',
+  'F',
+  'Fb',
+  'G',
+  'Gb',
+  'A',
+  'Ab',
+  'B'
+]
+
+export const majorScale = [0, 2, 4, 5, 7, 9, 11]
+export const minorScale = [0, 2, 3, 5, 7, 8, 10]
+
+export const scales = {
+  major: majorScale,
+  minor: minorScale
+}
+
+export const note2name = (note: number): string => {
+  const m = note % 12
+  const o = Math.floor(note / 12) - 2
+  return `${roots[m]}${o}`
+}
