230ffbfef9e5c6f66397c229ae83123df6a2d459
nishaya <nishaya@users.noreply.github.com>
2018-03-25 14:06:50 +0900

    splash

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import App from 'App'
+import SplashComponent from 'Components/Splash'
 import AuthContainer from 'Containers/Auth'
 import 'index.css'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import store from 'Redux/store'
 import { initFirebase } from 'Utils/firebase'
 
@@ -14,7 +17,12 @@ ReactDOM.render(
   <MuiThemeProvider>
     <Provider store={store}>
       <AuthContainer>
-        <App />
+        <BrowserRouter>
+          <div>
+            <Route path="/session" component={App} />
+            <Route component={SplashComponent} />
+          </div>
+        </BrowserRouter>
       </AuthContainer>
     </Provider>
   </MuiThemeProvider>,
