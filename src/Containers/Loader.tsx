da5825de50690b815c6a6ec919eb4b8859e5a97b
nishaya <nishaya@users.noreply.github.com>
2018-03-25 14:30:27 +0900

    routing

--- src/Containers/Loader.tsx
+++ src/Containers/Loader.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react'
-import { match } from 'react-router'
+import { match, withRouter } from 'react-router'
 
 interface Props {
   match?: match<{ sessionId?: string }>
+  history: any
 }
 
-const LoaderComponent = ({ match }: Props) => {
+const LoaderComponent = ({ match, history }: Props) => {
   console.log('loader params', match)
+  const sessionId =
+    match && match.params && match.params.sessionId
+      ? match.params.sessionId
+      : 'new'
+
+  if (!sessionId || sessionId == 'new') {
+    console.log('create new')
+    history.push(`/session/${sessionId}`)
+  } else {
+    console.log('load session')
+    history.push(`/session/${sessionId}`)
+  }
+  console.log('history', history)
   return <div>loader</div>
 }
-export default LoaderComponent
+export default withRouter(LoaderComponent)
