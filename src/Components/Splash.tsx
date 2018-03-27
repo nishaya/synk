448104a4c025b8cb86e2106eed8dedca3a2a8698
nishaya <nishaya@users.noreply.github.com>
2018-03-27 09:56:01 +0900

    create button

--- src/Components/Splash.tsx
+++ src/Components/Splash.tsx
@@ -1,12 +1,21 @@
 import Logo from 'Components/Common/Logo'
+// import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import * as React from 'react'
 import styled from 'styled-components'
 
+// const Button = RaisedButton
 const Splash = styled.div`
   height: 100%;
 
   .head {
     height: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .menu {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -17,6 +26,12 @@ const SplashComponent = () => (
   <Splash>
     <div className="head">
       <Logo />
+    </div>
+    <div className="menu">
+      <FlatButton
+        backgroundColor="rgba(255, 255, 255, 0.1)"
+        label="Create New"
+      />
     </div>
   </Splash>
 )
