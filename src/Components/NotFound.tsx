cb9433a3e914c7ed2a0f5df3bd1b3f3848a21f93
nishaya <nishaya@users.noreply.github.com>
2018-03-27 10:07:52 +0900

    not found

--- src/Components/NotFound.tsx
+++ src/Components/NotFound.tsx
@@ -1,5 +1,39 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
-const NotFoundComponent = () => <div>Not Found</div>
+const MainLogo = styled.div`
+  color: #f67280;
+  font-size: 64px;
+  letter-spacing: 0.1em;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 100;
+  line-height: 160px;
+`
+
+/*
+const TagLine = styled.div`
+  color: #c06c84;
+  font-size: 23px;
+  letter-spacing: 0.1em;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 100;
+`
+*/
+
+const Splash = styled.div`
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const NotFoundComponent = () => (
+  <Splash>
+    <div className="head">
+      <MainLogo>Not Found</MainLogo>
+    </div>
+  </Splash>
+)
 
 export default NotFoundComponent
