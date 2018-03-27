fe417a32d0e53814d659a80fff0d93fbdb533432
nishaya <nishaya@users.noreply.github.com>
2018-03-27 10:08:54 +0900

    modify font

--- src/Components/NotFound.tsx
+++ src/Components/NotFound.tsx
@@ -1,37 +1,27 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-const MainLogo = styled.div`
-  color: #f67280;
-  font-size: 64px;
-  letter-spacing: 0.1em;
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 100;
-  line-height: 160px;
-`
-
-/*
-const TagLine = styled.div`
-  color: #c06c84;
-  font-size: 23px;
-  letter-spacing: 0.1em;
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 100;
-`
-*/
-
 const Splash = styled.div`
   height: 100%;
 
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .text {
+    color: #f67280;
+    font-size: 64px;
+    letter-spacing: 0.1em;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 100;
+    line-height: 160px;
+  }
 `
 
 const NotFoundComponent = () => (
   <Splash>
     <div className="head">
-      <MainLogo>Not Found</MainLogo>
+      <div className="text">Not Found</div>
     </div>
   </Splash>
 )
