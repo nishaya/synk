d252ae6408443abc7db7a5e2e4161879cd4195c7
nishaya <nishaya@users.noreply.github.com>
2018-03-28 10:55:22 +0900

    preset edit

--- src/Components/Track/Preset/Osc.tsx
+++ src/Components/Track/Preset/Osc.tsx
@@ -1,21 +1,62 @@
 import SliderControl from 'Components/Common/SliderControl'
+import { merge } from 'lodash'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
 import * as React from 'react'
 import { OscSynthPreset } from 'types'
 
 interface Props {
   preset: OscSynthPreset
+  onChange: (preset: OscSynthPreset) => void
 }
 
-const OscPreset = ({ preset }: Props) => {
+const OscPreset = ({ preset, onChange }: Props) => {
   return (
     <div>
+      <DropDownMenu
+        onChange={(e: any, i: number, v: string) => {
+          onChange(merge({ oscillator: v }, preset))
+        }}
+      >
+        <MenuItem value="sawtooth" primaryText="SAW" />
+        <MenuItem value="square" primaryText="SQUARE" />
+        <MenuItem value="sine" primaryText="SINE" />
+        <MenuItem value="triangle" primaryText="TRIANGLE" />
+      </DropDownMenu>
       <SliderControl
         min={0}
         max={127}
-        value={0}
+        value={preset.aeg.attack}
         label="attack"
         onChange={(v: number) => {
-          console.log('chnage', v)
+          onChange(merge({ aeg: { attack: v } }, preset))
+        }}
+      />
+      <SliderControl
+        min={0}
+        max={127}
+        value={preset.aeg.decay}
+        label="attack"
+        onChange={(v: number) => {
+          onChange(merge({ aeg: { decay: v } }, preset))
+        }}
+      />
+      <SliderControl
+        min={0}
+        max={127}
+        value={preset.aeg.sustain}
+        label="attack"
+        onChange={(v: number) => {
+          onChange(merge({ aeg: { sustain: v } }, preset))
+        }}
+      />
+      <SliderControl
+        min={0}
+        max={127}
+        value={preset.aeg.release}
+        label="attack"
+        onChange={(v: number) => {
+          onChange(merge({ aeg: { release: v } }, preset))
         }}
       />
     </div>
