982fb8896e8a6b8b1aecc8e5a23316120a9096b5
nishaya <nishaya@users.noreply.github.com>
2018-03-29 14:14:19 +0900

    bpm slider

--- src/Components/Controls/Bpm.tsx
+++ src/Components/Controls/Bpm.tsx
@@ -1,5 +1,11 @@
 import { Mutations } from 'Containers/Session'
+import Slider from 'material-ui/Slider'
 import * as React from 'react'
+
+const sliderStyle = {
+  marginTop: 8,
+  marginBottom: 8
+}
 
 interface Props {
   bpm: number
@@ -7,7 +13,21 @@ interface Props {
 }
 
 const BpmComponent = ({ bpm }: Props) => {
-  return <div>bpm: {bpm}</div>
+  return (
+    <div style={{ width: 200 }}>
+      <div>bpm: {bpm}</div>
+      <Slider
+        sliderStyle={sliderStyle}
+        value={bpm}
+        max={240}
+        min={40}
+        step={1}
+        onChange={(e: any, v: number) => {
+          console.log('bpm changed', v)
+        }}
+      />
+    </div>
+  )
 }
 
 export default BpmComponent
